06a1d3cc5f3d2ed0be5fc857e320e5344e3de028
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-12 15:43:18 +0100

    create func to get square size by container width

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,7 @@
 const divContainer = document.querySelector(".container");
+
+function createGrid(n) {
+    divContainer.replaceChildren(); //remove all child divs
+    const squareSize = divContainer.clientWidth / n;
+    
+}
