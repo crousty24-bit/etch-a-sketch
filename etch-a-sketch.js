5eb1043c7349146dd2c1688e36c1fd4b5e4dd1ea
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-12 16:37:32 +0100

    add loop to add 16x16 square grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,5 +3,12 @@ const divContainer = document.querySelector(".container");
 function createGrid(n) {
     divContainer.replaceChildren(); //remove all child divs
     const squareSize = divContainer.clientWidth / n;
-    
+    for (let i = 0; i < n * n; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        divContainer.appendChild(square);
+    }
 }
+createGrid(16);
